75680d0c4e9d4d870c21856cb857f5843400b0f9
unknown <samin289@gmail.com>
2021-01-12 12:25:30 -0500

    commit

--- trade-stars/src/App.tsx
+++ trade-stars/src/App.tsx
@@ -5,16 +5,18 @@ import TradesmenCompanySignin from './components/register/TradesmenCompanySignin
 import { TradesmenRegister } from './components/register/TradesmenRegister';
 import { Register } from './CustomerComponent/CustomerRegister';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import CreateReview from './components/reviews/CreateReview';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Branch example</h1>
+        <h1>Trade Stars</h1>
        <Router>
        <Route path= "/" exact component = {TradesmenRegister} />
        <Route path= "/TradesmenDescriptionRegister" exact component = {TradesmenDescriptionRegister} />
        <Route path= "/TradesmenCompanySignin" exact component = {TradesmenCompanySignin} />
+       <Route path= "/CreateReview" exact component = {CreateReview} />
        </Router>
       </header>
     </div>

190935c638a3319988faca3cf136f227c042fe68
Matt Northmore <rmnorthmore@gmail.com>
2021-01-14 15:10:03 -0500

    Updating front end models to match backend models and DB relationships

---
+++ trade-stars/src/models/Company.ts
@@ -0,0 +1,8 @@
+import { User } from "./User"
+
+export class Company {
+    companyId: number
+    companyName: string
+    companyType: string
+    companyOwner: User 
+}
--- trade-stars/src/models/Review.ts
+++ trade-stars/src/models/Review.ts
@@ -1,1 +1,9 @@
-export{}
+import { Company } from "./Company"
+import { User } from "./User"
+
+export class Review {
+    reviewId: number
+    reviewText: string
+    reviewedBy: User
+    reviewedFor: Company
+}

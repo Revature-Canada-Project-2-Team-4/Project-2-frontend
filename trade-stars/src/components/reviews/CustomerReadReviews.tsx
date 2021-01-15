dcfa5d8025316e900ad7e5c176af4e92bc26b3f3
unknown <samin289@gmail.com>
2021-01-15 01:10:26 -0500

    customer read reviews

---
+++ trade-stars/src/components/reviews/CustomerReadReviews.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { Company } from '../../models/Company';
+import { Review } from '../../models/Review';
+import { User } from '../../models/User';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+} from "@material-ui/core";
+import makeStyles from '@material-ui/core/styles/makeStyles';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
+
+
+
+const useStyles = makeStyles((theme: Theme) => ({
+    root: {
+        minWidth: 275,
+    },
+}),
+);
+
+
+interface ICustomerReadReviewsProps {
+    updateCurrentCompany: (c: Company) => void;
+    currentCompany: Company;
+    updateCurrentUser: (u: User) => void;
+    currentUser: User;
+    updateCurrentCompanyReviewList: (cRL: Review[]) => void;
+    currentReadReviewsList: Review[];
+
+
+
+}
+
+
+export const CustomerReadReviews: React.FunctionComponent<ICustomerReadReviewsProps> = (props) => {
+
+    const [currentCompany] = useState(props.currentCompany);
+    const [currentUser] = useState(props.currentUser);
+    const [reviews, changeReviews] = useState(props.currentReadReviewsList);
+
+    const classes = useStyles();
+
+    useEffect(() => {
+        const getReview = await 
+    })
+
+
+    let reviewDisplay = reviews.map((rev) => {
+        return (
+            <Card className={classes.root}>
+                <CardContent>
+                    <li>{rev}</li>
+                </CardContent>
+            </Card>
+
+        )
+    })
+
+    return ( {reviewDisplay}
+    );
+}

a68025488334ad81ca1e90b9f262f86d9f113278
Matt Northmore <rmnorthmore@gmail.com>
2021-01-16 15:44:45 -0500

    Fixed appointment confirmed and cancel functionality

--- trade-stars/src/remote/trade-stars/ts-appointments-functions.ts
+++ trade-stars/src/remote/trade-stars/ts-appointments-functions.ts
@@ -1,4 +1,5 @@
 import { tradeStarApi } from '.';
+import { Appointment } from '../../models/Appointment';
 
 export const getAllAppointments = async () => {
     try {
@@ -33,6 +34,27 @@ export const getAllAppointmentsByCompanyId = async (companyId: number) => {
 export const getAllAppointmentsByCustomerId = async (customerId: number) => {
     try{
         let res = await tradeStarApi.get(`/appointments/customer/${customerId}`);
+        console.log(res.data);
+        return res.data;
+    }catch(e) {
+        console.log(e);
+        if(e.response){
+            throw new Error(e.response.data);
+        } else {
+            throw new Error("Unable to fetch all appointments")
+        }
+    }
+}
+
+export const updateAppointmentCompletedById = async (appointment: Appointment, isCompleted:boolean) => {
+
+    let updatedAppt = {
+        ...appointment,
+        appointmentCompleted: isCompleted
+    }
+
+    try {
+        let res = await tradeStarApi.put(`appointments/`, updatedAppt);
         console.log(res.data);
         return res.data;
     }catch(e) {

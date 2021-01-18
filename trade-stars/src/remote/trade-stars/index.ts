5b5286dbb632907b45e0b3fed76855458545fe4b
Matt Northmore <rmnorthmore@gmail.com>
2021-01-18 15:31:25 -0500

    Fixing build

--- trade-stars/src/remote/trade-stars/index.ts
+++ trade-stars/src/remote/trade-stars/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 //this is where we config every single request for a certain api
 export const  tradeStarApi = axios.create({
     //You might need to add project name at end of url
-    baseURL:"http://ec2-54-175-141-33.compute-1.amazonaws.com:10000/",
+    baseURL:"http://ec2-54-175-141-33.compute-1.amazonaws.com:10000",
     headers:{
         'Content-Type': "application/json"
     }
